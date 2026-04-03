784f049ad5e1f11e8d2775d2936e7d477b6f435f
elbekrahmonov175-jpg <elbekrahmonov175@gmail.com>
2026-04-03 11:19:13 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_PATH = os.getenv("DATABASE_PATH", "finance.db")
+DATABASE_PATH = os.getenv("DATABASE_PATH", "/app/data/finance.db")
 
 EXPENSE_CATEGORIES = ["Еда", "Транспорт", "Покупки", "Развлечения", "Коммунальные", "Другое"]
 INCOME_CATEGORIES = ["Зарплата", "Бизнес", "Подарок", "Другое"]

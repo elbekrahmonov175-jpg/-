79d3e0e3abc18729b9f1cd76e3e544df362c8a62
elbekrahmonov175-jpg <elbekrahmonov175@gmail.com>
2026-06-28 16:05:20 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_PATH = os.getenv("DATABASE_PATH", "/app/data/finance.db")
-
-EXPENSE_CATEGORIES = ["Еда", "Транспорт", "Покупки", "Развлечения", "Коммунальные", "Другое"]
-INCOME_CATEGORIES = ["Зарплата", "Бизнес", "Подарок", "Другое"]

26d967aed9f400ea16220c43b662fa6a4dbbe400
elbekrahmonov175-jpg <elbekrahmonov175@gmail.com>
2026-06-28 16:04:53 +0500

    Delete keyboards directory

--- keyboards/stats.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-def get_stats_keyboard() -> InlineKeyboardMarkup:
-    keyboard = [
-        [InlineKeyboardButton(text="📥 Скачать Excel", callback_data="download_excel")]
-    ]
-    return InlineKeyboardMarkup(inline_keyboard=keyboard)
